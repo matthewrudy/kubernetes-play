d3441e1425a86306a321695b0ea6a0a6a5937fd6
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2015-06-10 01:43:36 +0800

    better secrets

--- images/ruby-secrets/server.rb
+++ images/ruby-secrets/server.rb
@@ -4,8 +4,17 @@ require 'sinatra'
 SECRETS_DIR = "/etc/secrets"
 
 get '/' do
-  if Dir.exist?("/etc/secrets")
-    "Secrets:\n#{Dir["/etc/secrets/**/*"]}"
+  if Dir.exist?(SECRETS_DIR)
+    rtn = "Secrets:\n"
+    Dir["#{SECRETS_DIR}/**/*"].each do |path|
+      raw = File.read(path)
+      decoded = raw.unpack("m")
+
+      rtn << "  #{path}:"
+      rtn << "    raw: #{raw}"
+      rtn << "    decoded: #{decoded}"
+    end
+    rtn
   else
     "No secrets"
   end
